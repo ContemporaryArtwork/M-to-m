5f85f73b73064f7b010815fb5377f1cdde2bec74
ContemporaryArtwork <grugg@cock.li>
2015-10-06 08:04:10 -0400

    Update and rename Things.java to Test_1.java

--- src/test/java/java/Test_1.java
+++ src/test/java/java/Test_1.java
@@ -6,7 +6,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class Things {
+public class Test_1 {
     @Test
     public void testSomething() {
         //String workDir = System.getProperty("user.dir");
