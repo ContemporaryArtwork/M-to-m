3b16f49846b27e5f1152fdb813cdad912b5074fe
ContemporaryArtwork <grugg@cock.li>
2015-10-05 20:38:42 -0400

    Update MyClassTest.java

--- src/test/java/java/MyClassTest.java
+++ src/test/java/java/MyClassTest.java
@@ -11,7 +11,7 @@ public class MyClassTest {
     public void testSomething() {
         //String workDir = System.getProperty("user.dir");
         String file = "KLSadd.tex";
-        File myFile = new File("\\" + file);
+        File myFile = new File("src\\" + file);
         try{
             String output = new Scanner(myFile).useDelimiter("\\Z").next();
             String output1 = output;
