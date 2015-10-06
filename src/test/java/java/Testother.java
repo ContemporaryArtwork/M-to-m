7fb52fd2a9012f25d6b97b54356f90436a60c790
ContemporaryArtwork <grugg@cock.li>
2015-10-06 07:56:15 -0400

    Update and rename two.java to Testother.java

---
+++ src/test/java/java/Testother.java
@@ -0,0 +1,34 @@
+package mM;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Testother {
+    @Test
+    public void testSomething() {
+        //String workDir = System.getProperty("user.dir");
+        String file = "KLSadd.tex";
+        
+        
+        
+      
+      File myFile = new File("/home/travis/build/ContemporaryArtwork/M-to-m/" + file);
+        try{
+            String output = new Scanner(myFile).useDelimiter("\\Z").next();
+            String output1 = output;
+            output1 = output1.replace("M", "{&-}");
+            output1 = output1.replace("m", "M");
+            output1 = output1.replace("{&-}", "m");
+            System.out.println(output1);
+          if (output.equals(output1)){
+                System.out.println("\nnothing was done");
+            }
+        } catch (FileNotFoundException e){
+          System.out.println("please put KLSadd.tex in app directory!");
+        }
+
+    }
+}
