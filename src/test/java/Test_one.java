a6b3973754ac430479803bbabd056e43ac6e22ce
ContemporaryArtwork <grugg@cock.li>
2015-10-07 19:18:59 -0400

    Update Test_one.java

--- src/test/java/Test_one.java
+++ src/test/java/Test_one.java
@@ -1,35 +1,14 @@
-package mM;
 
-import org.junit.Test;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+    public class Test_one {
 
-public class Test_one {
-    public static void main(String[] args){
-        String file = "KLSadd.tex";
-        File myFile = new 
-File("/home/travis/build/ContemporaryArtwork/M-to-m/src/test/resources/" + file);
-        try{
-            String output = new Scanner(myFile).useDelimiter("\\Z").next();
-            String output1 = output;
-            output1 = output1.replace("M", "{&-}");
-            output1 = output1.replace("m", "M");
-            output1 = output1.replace("{&-}", "m");
-            System.out.println(output1);
-        if (output.equals(output1)){
-                System.out.println("\nnothing was done");
-            }
-        } catch (FileNotFoundException e){
-          System.out.println("please put KLSadd.tex in app directory!");
+        @Test
+        public void testSomething() {
+            one_main.main(new String[0]);
+
+
         }
-        
     }
-    @Test
-    public void testSomething() {
-        main(new String[0]);
-       
 
-    }
-}
+
+
