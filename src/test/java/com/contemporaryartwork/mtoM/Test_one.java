0f2c820639f7c8d24bf0487798d8ee4689a4dfb7
ContemporaryArtwork <grugg@cock.li>
2015-11-18 17:38:17 -0500

    Update Test_one.java

--- src/test/java/com/contemporaryartwork/mtoM/Test_one.java
+++ src/test/java/com/contemporaryartwork/mtoM/Test_one.java
@@ -1,10 +1,7 @@
 package com.contemporaryartwork.mtoM;
 import org.junit.Test;
 
-    public class Test_one {
-        public static void main(String input){
-            one_main.main(input);
-        }
+   
         @Test
         public void testSomething() {
             one_main.main("KlSadd.tex");
