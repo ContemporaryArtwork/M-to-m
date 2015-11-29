5ab30156108a5c712af665b4fb80f6f4623de566
ContemporaryArtwork <grugg@cock.li>
2015-11-29 15:45:51 -0500

    Update FileMakerLine.java
    updated the filemaker program to add words, paragraphs, and periods.

--- src/test/java/com/contemporaryartwork/mtoM/FileMakerLine.java
+++ src/test/java/com/contemporaryartwork/mtoM/FileMakerLine.java
@@ -9,13 +9,13 @@ public class FileMakerLine {
 	public static String q;
 	public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException{
 		PrintWriter writer;
-		String in = JOptionPane.showInputDialog(null,"input filesize in MB");
+		String in = JOptionPane.showInputDialog(null,"input approx filesize in MB");
 		String name = JOptionPane.showInputDialog(null, "input name");
 		int load = 0;
 		
 		int count = Integer.parseInt(in) * 200000;
 		writer  = new PrintWriter(name, "UTF-8");
-		int co = 0;
+		int co = 0, check =0, check2 = 0, check3 = 0;
 		
 		JOptionPane.showMessageDialog (null, "Loading...process will be finished momentarily", "", JOptionPane.INFORMATION_MESSAGE);
 
@@ -23,18 +23,61 @@ public class FileMakerLine {
 			if (co == 90){
 				writer.println(" ");
 				co = 0;
+				check2++;
+				int compare = 3 + ran(5);
+				if (check2 == 8){
+					check2 = 0;
+				}
+				if (check2 == compare){
+					check2 = 0;
+					writer.println(" ");
+				}
 			}
+			
 			for(int k = 0; k<5; k++){
 				int c = ran(2);
+				int d = ran(3) + 5;
+				int e = ran(5) + 6;
+				int SpaceCheck = ran(7);
 				if (c==0){
 					writer.print(Character.toChars(ran(26)+65));
 					co++;
+					check++;
 				}
 				else{
 					writer.print(Character.toChars(ran(26)+97));
 					co++;
+					check++;
 				}
-				
+				if (SpaceCheck == check){
+					check3++;
+					if (check3 == e){
+						writer.print(".");
+						check3 =0;
+					}
+					if (check3 > e){
+						check3 =0;
+						writer.print(".");
+					}
+					
+					writer.print(" ");
+					check = 0;
+				}
+				if (check > d){
+					check3++;
+					if (check3 == e){
+						writer.print(".");
+						check3 = 0;
+					}
+					if (check3 > e){
+						check3 =0;
+						writer.print(".");
+					}
+					
+					writer.print(" ");
+					check = 0;
+					
+				}
 				
 				
 	
