688e8aa5afc5dbbdcedbd57c16b6829e543aa0e1
ContemporaryArtwork <grugg@cock.li>
2015-11-30 22:55:07 -0500

    Update TestMethod.java

--- src/test/java/com/contemporaryartwork/mtoM/TestMethod.java
+++ src/test/java/com/contemporaryartwork/mtoM/TestMethod.java
@@ -1,12 +1,13 @@
 package com.contemporaryartwork.mtoM;
 import org.junit.Test;
+import java.io.IOException;
 public class TestMethod{
  
    
      
    
         @Test
-        public void testSomething() {
+        public void testSomething() throws IOException {
            String[] Input = {"KLSadd.tex"};
             MConvertStream.main(Input);
 
