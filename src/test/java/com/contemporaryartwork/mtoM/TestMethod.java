c86ccf62038ffd74d3a3abcf7f73a0b4cfba9ff7
ContemporaryArtwork <grugg@cock.li>
2015-12-07 20:08:39 -0500

    Update TestMethod.java

--- src/test/java/com/contemporaryartwork/mtoM/TestMethod.java
+++ src/test/java/com/contemporaryartwork/mtoM/TestMethod.java
@@ -1,6 +1,6 @@
 package com.contemporaryartwork.mtoM;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 public class TestMethod{
