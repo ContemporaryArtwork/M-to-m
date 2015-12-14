68a5c0dd7dec07dd3c2bf8a63cb1569a7106d269
ContemporaryArtwork <grugg@cock.li>
2015-12-13 19:54:19 -0500

    Update MConvertStream.java

--- src/main/java/com/contemporaryartwork/mtoM/MConvertStream.java
+++ src/main/java/com/contemporaryartwork/mtoM/MConvertStream.java
@@ -4,7 +4,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class Charstream {
+public class MConvertStream {
     public static void main(String[] args) throws IOException {
 
         FileReader inputStream = null;
